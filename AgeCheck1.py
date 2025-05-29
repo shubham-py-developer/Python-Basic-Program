4dd694263e2de1281549a183feed441e38a43df8
Shubham Prajapati <shubhprajapati042@gmail.com>
2025-05-29 23:58:32 +0530

    New branch created and modify the file

--- AgeCheck1.py
+++ AgeCheck1.py
@@ -1,8 +1,8 @@
 #Ask a user for age & check if they are a child, teenager, or adult.
 a=float(input("enter your age here:"))#here child is age group 0-14,teenager14-18,adult 19-above
 if a<=14:
-    print("you are a child")
+    print("you are a child ")
 elif a<=18:
-    print("you are a teenager")
+    print("you are a teenager ")
 else:
-    print("you are an adult")    
+    print("you are an adult ")    
