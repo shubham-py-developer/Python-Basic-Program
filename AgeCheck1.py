08ccd6864ef1657aa974383b4ed08a4de2648baf
shubham-py-developer <shubhprajapati042@gmail.com>
2025-08-24 13:52:58 +0530

    Update AgeCheck1.py
    with print update pattern

--- AgeCheck1.py
+++ AgeCheck1.py
@@ -1,8 +1,8 @@
 #Ask a user for age & check if they are a child, teenager, or adult.
 a=float(input("enter your age here:"))#here child is age group 0-14,teenager14-18,adult 19-above
 if a<=14:
-    print("you are a child")
+    print(f"you are a child beacause your age is {a}!")
 elif a<=18:
-    print("you are a teenager")
+    print(f"you are a teenagerbeacause your age is {a}!")
 else:
-    print("you are an adult")    
+    print("you are an adult beacause your age is {a}!")    
